6d21d9a1591954543034004983398fa111810758
John Duggan <dugganjw@ornl.gov>
2026-03-31 17:20:07 -0400

    cd to default_dir from NDIP tool XML

--- ndip/user_command.sh
+++ ndip/user_command.sh
@@ -3,4 +3,5 @@
 #User command script which is what will execute the desired user program.
 #Should be modified to run the desired program and remove this tail command.
 
+cd $DEFAULT_DIR
 pixi run -m /opt/NeuXtalViz/pixi.toml python /opt/NeuXtalViz/src/NeuXtalViz.py
