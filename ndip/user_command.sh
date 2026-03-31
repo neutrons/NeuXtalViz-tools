c24a58515ccb950643329b00fab6186482e166ab
John Duggan <dugganjw@ornl.gov>
2026-03-31 16:24:13 -0400

    Add noVNC build for NDIP

---
+++ ndip/user_command.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#User command script which is what will execute the desired user program.
+#Should be modified to run the desired program and remove this tail command.
+
+pixi run -m /opt/NeuXtalViz/pixi.toml python /opt/NeuXtalViz/src/NeuXtalViz.py
